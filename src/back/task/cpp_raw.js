6cd19079c7fb86fdbf4715acbad697d6b25889a0
buyoh <japanjetone@live.jp>
2019-06-30 11:44:38 +0900

    add cpp(use default g++, not wsl, cygwin)

---
+++ src/back/task/cpp_raw.js
@@ -0,0 +1,140 @@
+const myexec = require('./util/exec');
+const FileWrapper = require('./util/filewrapper');
+const DefaultTask = require('./util/default').generateDefaultTasks('cpp');
+const cpp = require('./cpp');
+
+// -------------------------------------
+
+// -------------------------------------
+
+exports.info = {
+    name: "C++",
+    editor: "c_cpp",
+    tabwidth: 4
+};
+
+const options = {
+    optimization: ["default", "-O3"],
+    std: ["-std=gnu++14", "-std=gnu++17", "-std=c++14", "-std=c++17"]
+};
+exports.options = options;
+
+// -------------------------------------
+
+exports.recipes = {
+    "compile > run": {
+        tasks: ["setupAll", "compile", "run"]
+    },
+    "run(no update)": {
+        tasks: ["setupIO", "run"]
+    }
+};
+
+// -------------------------------------
+
+/**
+ * @param {string} msg 
+ */
+function pickupInformations(msg) {
+    if (!msg) return [];
+    const infos = [];
+    for (let line of msg.split("\n")) {
+        const m = line.match(/^\.\/code\.cpp\:(\d+)\:(\d+)\: (\w+)\:/);
+        if (m) {
+            infos.push({
+                text: line,
+                row: +m[1] - 1,
+                column: +m[2] - 1,
+                type: m[3]
+            });
+        }
+    }
+    return infos;
+}
+exports.pickupInformations = pickupInformations;
+
+// -------------------------------------
+
+exports.command = {
+    /** setup files */
+    setupAll: cpp.command.setupAll,
+    setupIO: cpp.command.setupIO,
+
+    /** compile codes */
+    compile: function(task, callback) {
+        const cwdir = FileWrapper.getTempDirName(task.uniqueName);
+
+        Promise.resolve().then(() => {
+            return new Promise((resolve, reject) => {
+                let param = ["./code.cpp", "-o", "./code.out"];
+
+                if (task.json.options.optimization === "-O3") param.push("-O3");
+                if (options.std.includes(task.json.options.std)) param.push(task.json.options.std);
+                else param.push("-std=gnu++14");
+
+                let killer = myexec.spawn_fileio(
+                    "g++", param,
+                    null, cwdir + "/stdout.txt", cwdir + "/stderr.txt", {
+                        cwd: cwdir
+                    },
+                    (code, json) => {
+                        DefaultTask.util.promiseResultResponser(json, cwdir, callback, pickupInformations)
+                            .then(() => {
+                                if (code === 0) resolve();
+                                reject();
+                            });
+                    }
+                );
+
+                callback.call(null, "progress", {
+                    killer: killer
+                });
+            });
+
+        }).catch((e) => {
+            DefaultTask.util.errorHandler(e, callback);
+        });
+    },
+
+    /** run compiled file */
+    run: function(task, callback) {
+        const suffixs = Object.keys(task.json.txt_stdins);
+        const cwdir = FileWrapper.getTempDirName(task.uniqueName);
+
+        Promise.resolve().then(() => {
+            return DefaultTask.util.promiseMultiSeries(suffixs.map((e) => [e]), (suffix) => {
+                const nameStdin = "stdin" + suffix + ".txt";
+                const nameStdout = "stdout" + suffix + ".txt";
+                const nameStderr = "stderr" + suffix + ".txt";
+                return new Promise((resolve, reject) => {
+                    let killer = myexec.spawn_fileio(
+                        "./code.out", [],
+                        cwdir + "/" + nameStdin, cwdir + "/" + nameStdout, cwdir + "/" + nameStderr, {
+                            cwd: cwdir
+                        },
+                        (code, json) => {
+                            resolve(json);
+                        }
+                    );
+                    callback.call(null, "progress", {
+                        killer: killer
+                    });
+                }).then((json) => {
+                    json.key = suffix;
+                    return DefaultTask.util.promiseResultResponser(
+                        json, cwdir, callback, null,
+                        nameStdout, nameStderr, "ac", "wa"
+                    ).then(() => Promise.resolve(json.code));
+                });
+            });
+        }).then((args) => {
+            if (args.filter((e) => (e != 0)).length === 0)
+                callback.call(null, "continue");
+            else
+                callback.call(null, "failed");
+            return Promise.resolve();
+        }).catch((e) => {
+            DefaultTask.util.errorHandler(e, callback);
+        });
+    }
+};
