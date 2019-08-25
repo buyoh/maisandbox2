3494fab45aa693bb84c067fc611700f333e4871d
buyoh <japanjetone@live.jp>
2019-08-25 22:58:56 +0900

    apply eslint

--- test/back/exec.js
+++ test/back/exec.js
@@ -1,9 +1,10 @@
 const assert = require('assert');
 const fs = require('fs');
-const child_process = require('child_process');
+// const child_process = require('child_process');
 const mktmpdir = require('mktmpdir');
-var async = require('async');
+// var async = require('async');
 
+/* eslint-env mocha */
 
 function isExistFile(path){
     try{
@@ -16,96 +17,96 @@ function isExistFile(path){
 
 /* ------------------------- */
 
-const cygwin_env_path = ";C:/cygwin64/bin;C:/cygwin64/usr/local/bin;";
+const cygwin_env_path = ';C:/cygwin64/bin;C:/cygwin64/usr/local/bin;';
 
 /* ------------------------- */
 
 
-const testjs = require('../../src/back/exec.js');
+const testjs = require('../../src/back/task/util/exec.js');
 
-describe("test of exec.js", function(){
+describe('test of exec.js', function(){
 
     before(function(done){
-        testjs.exec("rm -rf ./temp", function(err, stdout, stderr){
-            fs.mkdir("temp", function(err){ done(); });
+        testjs.exec('rm -rf ./temp', function(err, stdout, stderr){
+            fs.mkdir('temp', function(err){ done(); });
         });
         
     });
     
-    it("rubyの実行結果が取得出来る(exec)", function(done){
-        testjs.exec("ruby -e 'p 1+8'", function(err, stdout, stderr){
+    it('rubyの実行結果が取得出来る(exec)', function(done){
+        testjs.exec('ruby -e \'p 1+8\'', function(err, stdout, stderr){
             assert.equal(!!err, false);
-            assert.equal(stdout.trim(), "9");
+            assert.equal(stdout.trim(), '9');
             done();
         });
     });
 
-    it("コマンドが存在しない時の挙動(exec)", function(done){
-        testjs.exec("my_nice_hoge", function(err, stdout, stderr){
+    it('コマンドが存在しない時の挙動(exec)', function(done){
+        testjs.exec('my_nice_hoge', function(err, stdout, stderr){
             assert.equal(!!err, true);
             done();
         });
     });
 
-    it("ファイル操作が出来る(exec)", function(done){
-        testjs.exec("rm ./__e_m_p_t_y__.txt", function(err, stdout, stderr){});
-        testjs.exec("touch ./__e_m_p_t_y__.txt", function(err, stdout, stderr){
-            assert.equal(!err, true, "touch(windowsにtouchは無い)");
-            assert.equal(isExistFile("./__e_m_p_t_y__.txt"), true, "touched");
-            testjs.exec("rm ./__e_m_p_t_y__.txt", function(err, stdout, stderr){
-                assert.equal(!err, true, "rm");
-                assert.equal(isExistFile("./__e_m_p_t_y__.txt"), false, "removed");
+    it('ファイル操作が出来る(exec)', function(done){
+        testjs.exec('rm ./__e_m_p_t_y__.txt', function(err, stdout, stderr){});
+        testjs.exec('touch ./__e_m_p_t_y__.txt', function(err, stdout, stderr){
+            assert.equal(!err, true, 'touch(windowsにtouchは無い)');
+            assert.equal(isExistFile('./__e_m_p_t_y__.txt'), true, 'touched');
+            testjs.exec('rm ./__e_m_p_t_y__.txt', function(err, stdout, stderr){
+                assert.equal(!err, true, 'rm');
+                assert.equal(isExistFile('./__e_m_p_t_y__.txt'), false, 'removed');
                 done();
             });
         });
     });
 
-    it("標準入出力を伴う実行が出来る(spawn_fileio)", function(done){
-        process.chdir("./temp");
+    it('標準入出力を伴う実行が出来る(spawn_fileio)', function(done){
+        process.chdir('./temp');
         mktmpdir(function(err, tempdir) {
             if (err) throw err;
             process.chdir(tempdir);
             
-            fs.writeFileSync("./test.rb","s=gets.chomp;puts s+'#out';STDERR.puts s+'#err'");
-            fs.writeFileSync("./in.txt","hello");
-            testjs.spawn_fileio("ruby", ["./test.rb"], "./in.txt", "./out.txt", "./err.txt", {}, function(code, json){
-                assert.equal(code, 0, "exitcode == 0");
-                let stdout = fs.readFileSync("./out.txt", 'UTF-8');
-                let stderr = fs.readFileSync("./err.txt", 'UTF-8');
+            fs.writeFileSync('./test.rb','s=gets.chomp;puts s+\'#out\';STDERR.puts s+\'#err\'');
+            fs.writeFileSync('./in.txt','hello');
+            testjs.spawn_fileio('ruby', ['./test.rb'], './in.txt', './out.txt', './err.txt', {}, function(code, json){
+                assert.equal(code, 0, 'exitcode == 0');
+                let stdout = fs.readFileSync('./out.txt', 'UTF-8');
+                let stderr = fs.readFileSync('./err.txt', 'UTF-8');
 
-                assert.equal(stdout.trim(), "hello#out", "check stdout");
-                assert.equal(stderr.trim(), "hello#err", "check stderr");
+                assert.equal(stdout.trim(), 'hello#out', 'check stdout');
+                assert.equal(stderr.trim(), 'hello#err', 'check stderr');
 
-                process.chdir("../");
-                testjs.exec("rm -rf " + tempdir, function(err, stdout, stderr){});
-                process.chdir("../");
+                process.chdir('../');
+                testjs.exec('rm -rf ' + tempdir, function(err, stdout, stderr){});
+                process.chdir('../');
                 done();
             });
         });
     });
 
-    it("rubyの実行結果が取得出来る(spawn_buff)", function(done){
-        testjs.spawn_buff("ruby", ["-e","'p 8+1'"], "", {}, function(err, code, signal, stdout, stderr){
-            assert.equal(err, null, "success");
-            assert.equal(code, 0, "exitcode == 0");
-            assert.equal(stdout.toString().trim(), "9", "check stdout");
-            assert.equal(stderr.toString().trim(), "", "check stderr");
+    it('rubyの実行結果が取得出来る(spawn_buff)', function(done){
+        testjs.spawn_buff('ruby', ['-e','\'p 8+1\''], '', {}, function(err, code, signal, stdout, stderr){
+            assert.equal(err, null, 'success');
+            assert.equal(code, 0, 'exitcode == 0');
+            assert.equal(stdout.toString().trim(), '9', 'check stdout');
+            assert.equal(stderr.toString().trim(), '', 'check stderr');
             done();
         });
     });
 
-    it("cygwin環境のg++が呼び出せる", function(done){
-        testjs.exec("g++ --version'", {env:{path: cygwin_env_path}}, function(err, stdout, stderr){
+    it('cygwin環境のg++が呼び出せる', function(done){
+        testjs.exec('g++ --version\'', {env:{path: cygwin_env_path}}, function(err, stdout, stderr){
             assert.equal(!!err, false);
-            assert.equal(stdout.substr(0,3), "g++", "check stdout(g++...)");
-            assert.equal(stderr.toString().trim(), "", "no stderr");
+            assert.equal(stdout.substr(0,3), 'g++', 'check stdout(g++...)');
+            assert.equal(stderr.toString().trim(), '', 'no stderr');
             done();
         });
     });
 
-    it("cygwin環境でC++をコンパイル・実行出来る", function(done){
+    it('cygwin環境でC++をコンパイル・実行出来る', function(done){
         this.timeout(120000);
-        process.chdir("./temp");
+        process.chdir('./temp');
         mktmpdir(function(err, tempdir) {
             if (err) throw err;
             process.chdir(tempdir);
@@ -114,25 +115,25 @@ describe("test of exec.js", function(){
                 env: {PATH: cygwin_env_path}
             };
         
-            fs.writeFileSync("./test.cpp",'#include<iostream>\nusing namespace std;\nint main(){string str;cin>>str;cout<<str<<" world"<<endl;return 0;}');
-            fs.writeFileSync("./in.txt","hello");
+            fs.writeFileSync('./test.cpp','#include<iostream>\nusing namespace std;\nint main(){string str;cin>>str;cout<<str<<" world"<<endl;return 0;}');
+            fs.writeFileSync('./in.txt','hello');
 
-            testjs.spawn_fileio("g++", ["./test.cpp","-std=gnu++14","-o","./test.out"], null, null, "./err.txt", options, function(code, json){
-                assert.equal(code, 0, "(compile) exitcode == 0");
-                assert.ok(isExistFile("./test.out"), "generated");
+            testjs.spawn_fileio('g++', ['./test.cpp','-std=gnu++14','-o','./test.out'], null, null, './err.txt', options, function(code, json){
+                assert.equal(code, 0, '(compile) exitcode == 0');
+                assert.ok(isExistFile('./test.out'), 'generated');
                 //let stderr = fs.readFileSync("./err.txt", 'UTF-8');
                 
-                testjs.spawn_fileio("./test.out", [], "./in.txt", "./out.txt", "./err.txt", options, function(code, json){
-                    assert.equal(code, 0, "exitcode == 0");
-                    let stdout = fs.readFileSync("./out.txt", 'UTF-8');
-                    let stderr = fs.readFileSync("./err.txt", 'UTF-8');
+                testjs.spawn_fileio('./test.out', [], './in.txt', './out.txt', './err.txt', options, function(code, json){
+                    assert.equal(code, 0, 'exitcode == 0');
+                    let stdout = fs.readFileSync('./out.txt', 'UTF-8');
+                    let stderr = fs.readFileSync('./err.txt', 'UTF-8');
         
-                    assert.equal(stdout.trim(), "hello world", "check stdout");
+                    assert.equal(stdout.trim(), 'hello world', 'check stdout');
                     // assert.equal(stderr.trim(), "hello#err", "check stderr");
         
-                    process.chdir("../");
-                    testjs.exec("rm -rf " + tempdir, function(err, stdout, stderr){});
-                    process.chdir("../");
+                    process.chdir('../');
+                    testjs.exec('rm -rf ' + tempdir, function(err, stdout, stderr){});
+                    process.chdir('../');
                     done();
                 });
             });
@@ -148,33 +149,33 @@ describe("test of exec.js", function(){
     //     });
     // });
     
-    it("標準入出力を伴う実行が出来る(spawn_fileio, bash)", function(done){
-        process.chdir("./temp");
+    it('標準入出力を伴う実行が出来る(spawn_fileio, bash)', function(done){
+        process.chdir('./temp');
         mktmpdir(function(err, tempdir) {
             if (err) throw err;
             process.chdir(tempdir);
-            fs.writeFileSync("./test.rb","s=gets.chomp;puts s+'#out';STDERR.puts s+'#err'");
-            fs.writeFileSync("./in.txt","hello");
-            testjs.spawn_fileio("bash", ["-c", "ruby ./test.rb < in.txt 1> out.txt 2> err.txt"], null, null, null, {}, function(code, json){
-                assert.equal(code, 0, "exitcode == 0");
-                let stdout = fs.readFileSync("./out.txt", 'UTF-8');
-                let stderr = fs.readFileSync("./err.txt", 'UTF-8');
+            fs.writeFileSync('./test.rb','s=gets.chomp;puts s+\'#out\';STDERR.puts s+\'#err\'');
+            fs.writeFileSync('./in.txt','hello');
+            testjs.spawn_fileio('bash', ['-c', 'ruby ./test.rb < in.txt 1> out.txt 2> err.txt'], null, null, null, {}, function(code, json){
+                assert.equal(code, 0, 'exitcode == 0');
+                let stdout = fs.readFileSync('./out.txt', 'UTF-8');
+                let stderr = fs.readFileSync('./err.txt', 'UTF-8');
 
-                assert.equal(stdout.trim(), "hello#out", "check stdout");
-                assert.equal(stderr.trim(), "hello#err", "check stderr");
+                assert.equal(stdout.trim(), 'hello#out', 'check stdout');
+                assert.equal(stderr.trim(), 'hello#err', 'check stderr');
 
-                process.chdir("../");
-                testjs.exec("rm -rf " + tempdir, function(err, stdout, stderr){});
-                process.chdir("../");
+                process.chdir('../');
+                testjs.exec('rm -rf ' + tempdir, function(err, stdout, stderr){});
+                process.chdir('../');
                 done();
             });
         });
     });
 
     
-    it("bash環境でC++をコンパイル・実行出来る", function(done){
+    it('bash環境でC++をコンパイル・実行出来る', function(done){
         this.timeout(120000);
-        process.chdir("./temp");
+        process.chdir('./temp');
         mktmpdir(function(err, tempdir) {
             if (err) throw err;
             process.chdir(tempdir);
@@ -183,25 +184,25 @@ describe("test of exec.js", function(){
                 env: {PATH: cygwin_env_path}
             };
         
-            fs.writeFileSync("./bashcpp.cpp",'#include<iostream>\nusing namespace std;\nint main(){string str;cin>>str;cout<<str<<" world"<<endl;return 0;}');
-            fs.writeFileSync("./in_bashcpp.txt","hello");
+            fs.writeFileSync('./bashcpp.cpp','#include<iostream>\nusing namespace std;\nint main(){string str;cin>>str;cout<<str<<" world"<<endl;return 0;}');
+            fs.writeFileSync('./in_bashcpp.txt','hello');
 
-            testjs.spawn_fileio("bash", ["-c", "g++ -std=gnu++14 ./bashcpp.cpp -o ./bashcpp.out 1> out_bashcpp.txt 2> err_bashcpp.txt"], null, null, null, options, function(code, json){
-                assert.equal(code, 0, "(compile) exitcode == 0");
-                assert.ok(isExistFile("./bashcpp.out"), "generated");
+            testjs.spawn_fileio('bash', ['-c', 'g++ -std=gnu++14 ./bashcpp.cpp -o ./bashcpp.out 1> out_bashcpp.txt 2> err_bashcpp.txt'], null, null, null, options, function(code, json){
+                assert.equal(code, 0, '(compile) exitcode == 0');
+                assert.ok(isExistFile('./bashcpp.out'), 'generated');
                 //let stderr = fs.readFileSync("./err.txt", 'UTF-8');
                 
-                testjs.spawn_fileio("bash", ["-c", "./bashcpp.out < ./in_bashcpp.txt 1> out_bashcpp.txt 2> err_bashcpp.txt"], null, null, null, options, function(code, json){
-                    assert.equal(code, 0, "exitcode == 0");
-                    let stdout = fs.readFileSync("./out_bashcpp.txt", 'UTF-8');
-                    let stderr = fs.readFileSync("./err_bashcpp.txt", 'UTF-8');
+                testjs.spawn_fileio('bash', ['-c', './bashcpp.out < ./in_bashcpp.txt 1> out_bashcpp.txt 2> err_bashcpp.txt'], null, null, null, options, function(code, json){
+                    assert.equal(code, 0, 'exitcode == 0');
+                    let stdout = fs.readFileSync('./out_bashcpp.txt', 'UTF-8');
+                    let stderr = fs.readFileSync('./err_bashcpp.txt', 'UTF-8');
         
-                    assert.equal(stdout.trim(), "hello world", "check stdout");
+                    assert.equal(stdout.trim(), 'hello world', 'check stdout');
                     // assert.equal(stderr.trim(), "hello#err", "check stderr");
         
-                    process.chdir("../");
-                    testjs.exec("rm -rf " + tempdir, function(err, stdout, stderr){});
-                    process.chdir("../");
+                    process.chdir('../');
+                    testjs.exec('rm -rf ' + tempdir, function(err, stdout, stderr){});
+                    process.chdir('../');
                     done();
                 });
             });
--- test/back/taskexecutor.js
+++ test/back/taskexecutor.js
@@ -1,60 +1,61 @@
 const assert = require('assert');
-const fs = require('fs');
+// const fs = require('fs');
 
+/* eslint-env mocha */
 
 
 const testjs = require('../../src/back/taskexecutor.js');
 
-describe("test of taskexecutor.js", function(){
+describe('test of taskexecutor.js', function(){
     
-    it("hello world", function(done){
+    it('hello world', function(done){
         const info = {
-            txt_stdin: "",
-            txt_code: "puts 'hello world'",
-            cmd: "Ruby"
+            txt_stdin: '',
+            txt_code: 'puts \'hello world\'',
+            cmd: 'Ruby'
         };
         let state = 0;
         testjs.pushTask(info, function(type, json){
             if (state === 0){
-                assert.equal(type, "prepare", "準備開始");
+                assert.equal(type, 'prepare', '準備開始');
             }else if (state === 1){
-                assert.equal(type, "compile", "syntax");
+                assert.equal(type, 'compile', 'syntax');
             }else if (state === 2){
-                assert.equal(type, "execute", "実行開始");
+                assert.equal(type, 'execute', '実行開始');
             }else if (state === 3){
-                assert.equal(type, "success", "プロセスは終了した");
-                assert.equal(json.code, 0, "プロセスは正常終了した");
-                assert.equal(json.stderr.trim(), "", "標準エラー");
-                assert.equal(json.stdout.trim(), "hello world", "標準出力");
+                assert.equal(type, 'success', 'プロセスは終了した');
+                assert.equal(json.code, 0, 'プロセスは正常終了した');
+                assert.equal(json.stderr.trim(), '', '標準エラー');
+                assert.equal(json.stdout.trim(), 'hello world', '標準出力');
                 done();
             }else{
-                assert.fail(""+(state+1)+"回目のcallbackでtype("+type+")を得た");
+                assert.fail(''+(state+1)+'回目のcallbackでtype('+type+')を得た');
             }
             ++state;
         });
     });
 
-    it("kill", function(done){
+    it('kill', function(done){
         const info = {
-            txt_stdin: "",
-            txt_code: "sleep 5;puts 'goodmorning'",
-            cmd: "Ruby"
+            txt_stdin: '',
+            txt_code: 'sleep 5;puts \'goodmorning\'',
+            cmd: 'Ruby'
         };
         let state = 0;
         testjs.pushTask(info, function(type, json){
             if (state === 0){
-                assert.equal(type, "prepare", "準備開始");
+                assert.equal(type, 'prepare', '準備開始');
             }else if (state === 1){
-                assert.equal(type, "compile", "syntax");
+                assert.equal(type, 'compile', 'syntax');
             }else if (state === 2){
-                assert.equal(type, "execute", "実行開始");
+                assert.equal(type, 'execute', '実行開始');
                 json.killer();
             }else if (state === 3){
-                assert.equal(type, "success", "プロセスは終了した");
-                assert.notEqual(json.code, 0, "プロセスは異常終了した");
+                assert.equal(type, 'success', 'プロセスは終了した');
+                assert.notEqual(json.code, 0, 'プロセスは異常終了した');
                 done();
             }else{
-                assert.fail(""+(state+1)+"回目のcallbackでtype("+type+")を得た");
+                assert.fail(''+(state+1)+'回目のcallbackでtype('+type+')を得た');
             }
             ++state;
         });
