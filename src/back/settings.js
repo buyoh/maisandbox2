4ef8d2a3d689fa67bfeb8a087b3960f5490c4159
舞葉 <japanjetone@live.jp>
2019-09-21 23:58:10 +0900

    use argv (#45)

--- src/back/settings.js
+++ src/back/settings.js
@@ -1,4 +1,6 @@
 
+const argv = require('./argv');
+
 const defaultSettings = {
     portno: 11450,
     hostname: 'localhost'
@@ -7,12 +9,7 @@ const defaultSettings = {
 exports.settings = (()=>{
     const settings = Object.assign({}, defaultSettings);
     
-    // read argv
-    for (let i = 2, m = null; i < process.argv.length; ++i) {
-        const arg = process.argv[i];
-        if (arg == '--port') m = 1;
-        else if (m == 1) settings.portno = +arg, m = null;
-    }
+    if (argv['--port']) settings.portno = parseInt(argv['--port']);
 
     return settings;
 })();
