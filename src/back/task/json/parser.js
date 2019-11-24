e882f627d666a71ac1b833aeca1a3ff02d918839
buyoh <japanjetone@live.jp>
2019-11-25 00:18:31 +0900

    wip

--- src/back/task/json/parser.js
+++ src/back/task/json/parser.js
@@ -18,15 +18,21 @@ function replaceArgument(elem, task) {
     else return elem;
 }
 
+// TODO: task, cwdir, callback ってどうやって渡すんや
+
+// return (task, cwdir, callback) => Promise
 function parseAction(action) {
     if (action.cmd === 'exec') {
         return (task, cwdir, callback) => {
             return new Promise((resolve, reject) => {
                 const param = action.args.map(e => replaceArgument(e, task));
 
+                const fin = action.stdin ? cwdir + replaceArgument(action.stdin, task) : null;
+                const fout = action.stdout ? cwdir + replaceArgument(action.stdout, task) : null;
+                const ferr = action.stderr ? cwdir + replaceArgument(action.stderr, task) : null;
+
                 let killer = Exec.spawn_fileio(
-                    action.cmd, param,
-                    null, cwdir + '/stdout.txt', cwdir + '/stderr.txt', {
+                    action.cmd, param, fin, fout, ferr, {
                     // env: {
                     //     PATH: FileWrapper.cygwinEnvPath
                     // },
@@ -34,10 +40,7 @@ function parseAction(action) {
                     },
                     (code, json) => {
                         DefaultTask.util.promiseResultResponser(json, cwdir, callback, pickupInformations)
-                            .then(() => {
-                                if (code === 0) resolve();
-                                reject();
-                            });
+                            .then(() => (code === 0) ? resolve() : reject());
                     }
                 );
 
@@ -55,6 +58,21 @@ function parseAction(action) {
     }
 }
 
+// TODO: solve eachcase
+
+// return (task, callback) => Promise
+function parseActions(actions) {
+    const pp = actions.map((a) => parseActions(a));
+    return (task, callback) => {
+        const cwdir = FileWrapper.getTempDirName(task.uniqueName);
+        // construct Promise
+        const promise = pp.reduce((p, action) => (p.then(action(task, cwdir, callback))), Promise.resolve());
+        promise.catch((e) => {
+            DefaultTask.util.errorHandler(e, callback);
+        });
+    };
+}
+
 fs.readFile('cpp.json', (err, data) => {
     if (err) return;
     const json = JSON.parse(data);
@@ -65,7 +83,7 @@ fs.readFile('cpp.json', (err, data) => {
     exports.recipes = json.recipes;
     exports.command = {}; // TODO: rename command to commands
 
-    for (let cmdname in json.command) {
+    for (let cmdname in json.commands) {
         const js = json.command[cmdname];
         if (js === 'default') {
             // TODO: 
